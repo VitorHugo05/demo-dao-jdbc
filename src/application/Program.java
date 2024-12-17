e587c760fdce0ebdfe8150db4dfb5f4d7f852bc8
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2024-12-17 20:45:48 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,11 @@ public class Program {
         Seller newSeller = new Seller(department, 4000.0, new Date(), "greg@gmail.com", "Greg", null);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
