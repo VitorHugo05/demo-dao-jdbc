5964e9119a4b0bf5d046452f9508a4571ab82776
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2024-12-16 21:48:49 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,9 @@ public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         Seller seller = new Seller(department, 3000.00, new Date(), "bob@gmail.com", "Bob", 21);
+
+        SellerDao sellerDao = DaoFactory.createSellerDAO();
+
         System.out.println(seller);
     }
 }
